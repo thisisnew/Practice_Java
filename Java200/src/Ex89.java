14312ef5fcf832433d3f212a5b8a508e96b31006
Kimdonghyun <thisisnew@navercom>
2020-07-28 22:33:00 +0900

    practice java...

--- Java200/src/Ex89.java
+++ Java200/src/Ex89.java
@@ -1,20 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class Ex89 {
 	public static void main(String[] args) {
-		String url = "https://www.billboard.com/charts/the-billboard-hot-100";
+		String newUrls = "https://www.billboard.com/charts/the-billboard-hot-100";
+		URL url = null;
 		
 		try {
+			url = new URL(newUrls);
 			
-			URLConnection urlConnection = new URL(url).openConnection();
-	        urlConnection.addRequestProperty("User-Agent", "Mozilla");
-	        urlConnection.setReadTimeout(5000);
-	        urlConnection.setConnectTimeout(5000);
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream(), "euc-kr"),8);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "euc-kr"),8);
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				if(!line.trim().equals("")) {
